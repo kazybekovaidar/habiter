3d6bee7d364a73806d7eb0e28a4b752b96b3dd46
Aidar Kazybekov <kazybekov_a@auca.kg>
2023-01-12 00:58:19 +0600

    Initial commit

---
+++ src/main/java/habiter/habiter/HabiterApplication.java
@@ -0,0 +1,13 @@
+package habiter.habiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/habiter/habiter/HabiterApplicationTests.java
@@ -0,0 +1,13 @@
+package habiter.habiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
